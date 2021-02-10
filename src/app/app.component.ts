3fe7fa1154f6c17562d773f1c1434d04d84a8571
bankwitthawat <lee.witthawat@outlook.co.th>
2021-02-10 10:53:31 +0700

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'pretest-q2';
+
+  keyword?: string = "";
+
+  category: any;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.initialData();
+  }
+
+  initialData(): void {
+    this.getCategory().subscribe(res =>{ this.category = res});
+  }
+
+
+  getCategory(): Observable<any> {
+    return this.http.get<any>("https://api.publicapis.org/categories");
+  }
 }
